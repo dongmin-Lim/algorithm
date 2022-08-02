cfae69d71b0b1e24ad4f11c66fa830db0f768b86
임동민 <limdongmin@imdongmin-ui-MacBookPro.local>
2022-08-02 17:49:29 +0900

    백준 11047 완료 하지만 틀렸습니다 라고 나옴.

--- 0801/11047.js
+++ 0801/11047.js
@@ -1,15 +1,22 @@
-let input = require("fs").readFileSync("example.txt").toString().split("\n");
+// let input = require("fs").readFileSync("example.txt").toString().split("\n");
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const num = Number(input[0].split(" ")[0]);
-const price = Number(input[0].split(" ")[1]);
-let rest = 0;
+const num = Number(input[0].split(" ")[0]); // 10
+const price = Number(input[0].split(" ")[1]); // 4200
+let rest;
+let count = 0;
 
 const result = (money) => {
   for (let i = num; i >= 1; i--) {
-    if (price > input[i]) {
-      console.log(parseInt(price / input[i]));
+    if (money > input[i]) {
+      let money_num = parseInt(money / input[i]); // 해당 단위로 나오는 갯수
+      rest = money % input[i]; // 나머지
+      count += money_num;
+      result(rest);
     }
+    if (rest == 0) break;
   }
 };
 
 result(price);
+console.log(count);
