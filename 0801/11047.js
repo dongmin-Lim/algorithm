51664d9cc1a657574b3507bd12b1fc4ffb68fc02
임동민 <limdongmin@imdongmin-ui-MacBookPro.local>
2022-08-03 01:13:14 +0900

    Complete 0801/11047.js

--- 0801/11047.js
+++ 0801/11047.js
@@ -1,22 +1,17 @@
-// let input = require("fs").readFileSync("example.txt").toString().split("\n");
-const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+let input = require("fs").readFileSync("example.txt").toString().trim().split("\n");
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const num = Number(input[0].split(" ")[0]); // 10
-const price = Number(input[0].split(" ")[1]); // 4200
-let rest;
+let price = Number(input[0].split(" ")[1]); // 4200
 let count = 0;
 
-const result = (money) => {
+const result = () => {
   for (let i = num; i >= 1; i--) {
-    if (money > input[i]) {
-      let money_num = parseInt(money / input[i]); // 해당 단위로 나오는 갯수
-      rest = money % input[i]; // 나머지
-      count += money_num;
-      result(rest);
-    }
-    if (rest == 0) break;
+    let money_num = parseInt(price / input[i]); // 몫
+    price = price % input[i]; // 나머지
+    count += money_num;
   }
 };
 
-result(price);
+result();
 console.log(count);
